194a725e30f21da7f080f2b0d14d8e77c900c048
Nandang <nandang@pasarhosting.co.id>
2018-08-07 09:33:57 +0700

    pemisahaan antara metode broca dan bmi

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -2,15 +2,28 @@ class CalcController < ApplicationController
   def index
   end
 
-  def new
-		#render plain: params.inspect
-      if params[:gender] == 'pria'
-        @result = (params[:tinggi].to_i - 100) - ((params[:tinggi].to_i - 100) * 0.1)
-        @bmi = @result / ((params[:tinggi].to_f / 100) ** 2)
-      else
-        @result = (params[:tinggi].to_i - 100) - ((params[:tinggi].to_i - 100) * 0.15)
-        @bmi = @result / ((params[:tinggi].to_f / 100) ** 2)
-      end
-			render :index
+  def broca 
+    if params[:gender] == 'pria'
+      @result = (params[:tinggi].to_i - 100) - ((params[:tinggi].to_i - 100) * 0.1)
+    else
+      @result = (params[:tinggi].to_i - 100) - ((params[:tinggi].to_i - 100) * 0.15)
+    end
+    render :index
+  end
+
+  def bmi 
+    @bmi = params[:berat_badan].to_f / ((params[:tinggi].to_f / 100) ** 2)
+    
+    if @bmi >= 30.0 
+      @status_bb = 'Kegemukan (Obesitas)'
+    elsif @bmi >= 25.0
+      @status_bb = 'Kelebihan berat badan'
+    elsif @bmi >= 18.5
+      @status_bb = 'Normal (Ideal)'
+    else
+      @status_bb = 'Kurus / Kekurang berat badan'
+    end
+
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'calc#index'
-  resources :calc, only: [:index, :new]
+  resources :calc, only: [:index, :new] do
+    get 'broca', on: :collection
+    get 'bmi', on: :collection
+  end
 end
